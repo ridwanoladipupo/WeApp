9fae0e9e7c3506ac35c59b421ad6ad0162dd912e
Fadil Oderinu <fadiloderinu@gmail.com>
2022-04-04 15:24:00 +0100

    Added Homepage*

--- scripts.js
+++ scripts.js
@@ -75,6 +75,8 @@ setInterval(() => {
     fourthEl.innerHTML = `${hoursIn24hrFormat + 4}` + `<span id="am-pm"> ${ampm}</span>`
 }, 10);
 
+
+
 let weather = {
     API_KEY: 'a97a1d104f9476af1023b6d871003eb9',
     fetchWeather: function (city) {
@@ -84,53 +86,42 @@ let weather = {
             + "&units=metric&appid="
             + this.API_KEY
         )
+
             .then((response) => response.json())
             .then((data) => this.displayWeather(data));
+
     },
     displayWeather: function (data) {
         const { name } = data;
         const { icon, description } = data.weather[0];
-        const { temp, humidity, temp_min, temp_max, pressure } = data.main;
+        const { humidity, temp_min, temp_max, pressure } = data.main;
         const { speed } = data.wind;
         const { sunrise, sunset, country } = data.sys;
-        console.log(name, icon, description, temp, humidity, speed, temp_min, temp_max, sunrise, sunset, country, pressure);
+        console.log(name, icon, description, humidity, speed, temp_min, temp_max, sunrise, sunset, country, pressure);
         document.querySelector(".location").innerText = name + ", " + country;
         document.querySelector(".today-sun").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
         document.querySelector(".remark").innerHTML = `${description[0].toUpperCase()}${description.slice(1)}`
-        document.querySelector(".day-temp").innerText = Math.floor(temp) + '°';
+        // document.querySelector(".day-temp").innerText = Math.floor(temp) + '°';
         document.querySelector(".wind").innerText = `${Math.floor(speed * 3.6)}` + 'km/h';
         document.querySelector(".humidity").innerText = humidity + '%';
         document.querySelector(".high-low").innerText = Math.floor(temp_max) + '°/' + Math.floor(temp_min) + '°';
         document.querySelector(".pressure").innerText = pressure + 'hPa';
         document.querySelector(".sunrise").innerText = `${window.moment(sunrise * 1000).format("HH:mm a")}`;
         document.querySelector(".sunset").innerText = `${window.moment(sunset * 1000).format("HH:mm a")}`;
+        document.querySelector(".weather").classList.remove("loading")
     },
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
     },
 };
 
-document.querySelector(".search button").addEventListener("click", function () {
-    weather.search();
-});
-
-document.querySelector(".search-bar").addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-        weather.search();
-    }
-});
+weather.fetchWeather("Lagos, NG")
 
 
 // "https://api.weatherbit.io/v2.0/history/hourly?&city=lagos&start_date=2022-03-31&end_date=2022-04-01&tz=local&key=063c3486a1714bb98e5ae5ada544cab6"
 //     + this.api_key
 
 
-
-
-
-// visualcrossing api key: ZMV9HEXU664HPKVSRNLB93LQE
-
-// https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Lagos,NG/2022-03-31T13:00:00?key=ZMV9HEXU664HPKVSRNLB93LQE&unitGroup=metric
 
 // https://api.openweathermap.org/data/2.5/forecast?q=lagos&units=metric&appid=a97a1d104f9476af1023b6d871003eb9
 let icon = {
@@ -150,17 +141,20 @@ let icon = {
                 let { icon, description } = dataas.list[i].weather[0]
                 document.querySelector(".day-one").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
                 document.querySelector(".day1-remark").innerText = `${description[0].toUpperCase()}${description.slice(1)}`;
+                document.querySelector(".weather").classList.remove("loading")
             }
             else if (i === 16) {
                 let { icon, description } = dataas.list[i].weather[0]
                 document.querySelector(".day-two").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
                 document.querySelector(".day2-remark").innerText = `${description[0].toUpperCase()}${description.slice(1)}`;
+                document.querySelector(".weather").classList.remove("loading")
             }
 
             else if (i === 24) {
                 let { icon, description } = dataas.list[i].weather[0]
                 document.querySelector(".day-three").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
                 document.querySelector(".day3-remark").innerText = `${description[0].toUpperCase()}${description.slice(1)}`;
+                document.querySelector(".weather").classList.remove("loading")
             }
         }
     },
@@ -169,15 +163,8 @@ let icon = {
     },
 };
 
-document.querySelector(".search button").addEventListener("click", function () {
-    icon.search();
-});
+icon.fetchIcon("Lagos, NG")
 
-document.querySelector(".search-bar").addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-        icon.search();
-    }
-});
 
 
 // https://api.weatherbit.io/v2.0/forecast/hourly?city=Raleigh,NC&key=API_KEY&hours=48
@@ -203,6 +190,7 @@ let hour = {
                 document.querySelector(".first-temp").innerText = Math.floor(temp) + '°';
                 document.querySelector(".first-cloud").innerText = clouds + '%';
                 document.querySelector(".first-uv").innerText = Math.floor(uv) + ' out of 10';
+                document.querySelector(".weather").classList.remove("loading")
             }
             else if (i === 1) {
                 let { temp, clouds, uv } = datas.data[i];
@@ -211,6 +199,7 @@ let hour = {
                 document.querySelector(".second-temp").innerText = Math.floor(temp) + '°';
                 document.querySelector(".second-cloud").innerText = clouds + '%';
                 document.querySelector(".second-uv").innerText = Math.floor(uv) + ' out of 10';
+                document.querySelector(".weather").classList.remove("loading")
             }
             else if (i === 2) {
                 let { temp, clouds, uv } = datas.data[i];
@@ -219,6 +208,7 @@ let hour = {
                 document.querySelector(".third-temp").innerText = Math.floor(temp) + '°';
                 document.querySelector(".third-cloud").innerText = clouds + '%';
                 document.querySelector(".third-uv").innerText = Math.floor(uv) + ' out of 10';
+                document.querySelector(".weather").classList.remove("loading")
             }
             else if (i === 3) {
                 let { temp, clouds, uv } = datas.data[i];
@@ -227,6 +217,7 @@ let hour = {
                 document.querySelector(".fourth-temp").innerText = Math.floor(temp) + '°';
                 document.querySelector(".fourth-cloud").innerText = clouds + '%';
                 document.querySelector(".fourth-uv").innerText = Math.floor(uv) + ' out of 10';
+                document.querySelector(".weather").classList.remove("loading")
             }
         }
     },
@@ -235,15 +226,10 @@ let hour = {
     },
 };
 
-document.querySelector(".search button").addEventListener("click", function () {
-    hour.search();
-});
+hour.fetchTemp("Lagos, NG")
 
-document.querySelector(".search-bar").addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-        hour.search();
-    }
-});
+
+
 
 
 // http://api.weatherapi.com/v1/forecast.json?key=8a51bec509ac40fd856133327223003&q=Lagos&days=3&aqi=no&alerts=no
@@ -260,8 +246,12 @@ let day = {
             .then((datass) => this.displayDay(datass));
     },
     displayDay: function (datass) {
-        for (let i = 1; i <= 3; i++) {
-            if (i === 1) {
+        for (let i = 0; i <= 3; i++) {
+            if (i === 0) {
+                let { temp } = datass.data[i];
+                document.querySelector(".day-temp").innerText = Math.floor(temp) + '°';
+            }
+            else if (i === 1) {
                 let { temp, wind_spd, datetime, sunrise_ts, sunset_ts, pres, rh } = datass.data[i];
                 document.querySelector(".day1-temp").innerText = Math.floor(temp) + '°';
                 document.querySelector(".day1-wind").innerText = Math.floor(wind_spd * 3.6) + 'km/h';
@@ -270,6 +260,7 @@ let day = {
                 document.querySelector(".day1-sunrise").innerText = `${window.moment(sunrise_ts * 1000).format("HH:mm a")}`;
                 document.querySelector(".day1-sunset").innerText = `${window.moment(sunset_ts * 1000).format("HH:mm a")}`;
                 document.querySelector(".day1-date").innerText = datetime;
+                document.querySelector(".weather").classList.remove("loading")
             }
             else if (i === 2) {
                 let { temp, wind_spd, datetime, sunrise_ts, sunset_ts, pres, rh } = datass.data[i];
@@ -280,6 +271,7 @@ let day = {
                 document.querySelector(".day2-sunrise").innerText = `${window.moment(sunrise_ts * 1000).format("HH:mm a")}`;
                 document.querySelector(".day2-sunset").innerText = `${window.moment(sunset_ts * 1000).format("HH:mm a")}`;
                 document.querySelector(".day2-date").innerText = datetime;
+                document.querySelector(".weather").classList.remove("loading")
             }
             else if (i === 3) {
                 let { temp, wind_spd, datetime, sunrise_ts, sunset_ts, pres, rh } = datass.data[i];
@@ -290,6 +282,7 @@ let day = {
                 document.querySelector(".day3-sunrise").innerText = `${window.moment(sunrise_ts * 1000).format("HH:mm a")}`;
                 document.querySelector(".day3-sunset").innerText = `${window.moment(sunset_ts * 1000).format("HH:mm a")}`;
                 document.querySelector(".day3-date").innerText = datetime;
+                document.querySelector(".weather").classList.remove("loading")
             }
         }
     },
@@ -297,17 +290,59 @@ let day = {
         this.fetchDay(document.querySelector(".search-bar").value);
     },
 };
-document.querySelector(".search button").addEventListener("click", function () {
-    day.search();
-});
 
-document.querySelector(".search-bar").addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-        day.search();
-    }
-});
+day.fetchDay("Lagos, NG")
 
-
-// https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Lagos/next3days?key=ZMV9HEXU664HPKVSRNLB93LQE
 
 // http://openweathermap.org/img/wn/04d@2x.png
+// weather.fetchWeather(`${window.navigator.geolocation.getCurrentPosition}`)
+// hour.fetchTemp(`${window.navigator.geolocation.getCurrentPosition}`)
+// day.fetchDay(`${window.navigator.geolocation.getCurrentPosition}`)
+// icon.fetchIcon(`${window.navigator.geolocation.getCurrentPosition}`)
+
+function trigger() {
+    document.querySelector(".search button").addEventListener("click", function () {
+        weather.search();
+    });
+
+    document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+    });
+    // 
+    document.querySelector(".search button").addEventListener("click", function () {
+        icon.search();
+    });
+
+    document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            icon.search();
+        }
+    });
+    // 
+    document.querySelector(".search button").addEventListener("click", function () {
+        day.search();
+    });
+
+    document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            day.search();
+        }
+    });
+    // 
+    document.querySelector(".search button").addEventListener("click", function () {
+        hour.search();
+    });
+
+    document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            hour.search();
+        }
+    });
+}
+
+trigger()
+
+
+
